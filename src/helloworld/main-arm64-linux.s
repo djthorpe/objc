7f3efb477847678bcba2be504dd46a7c64fd1c93
David Thorpe <djt@mutablelogic.com>
2025-06-21 09:23:00 +0200

    Updated

--- src/helloworld/main-arm64-linux.s
+++ src/helloworld/main-arm64-linux.s
@@ -1,21 +1,23 @@
-// Assembler program to print "Hello World!"
-// to stdout.
+// Assembler program to print "Hello World!" to stdout.
+///////////////////////////////////////////////////////////////////////////////
 
-.global main
+.text // Code section
+.global _start
 .align 4
 
-// Print hello world
-main: 
+_start: 
+  // Print hello world
   mov	  X0, #1	            // 1 = stdout
   adr	  X1, helloworld      // string to print
-  mov	  X2, #13	            // length of our string
+  mov	  X2, #len	          // length of our string
   mov	  X8, #64	            // write system call
   svc   #0x00               // Call kernel to output the string
 
-// Exit the program
+  // Exit the program
   mov   X0, #0              // Use 0 return code
-  mov   X9, #93             // call number 93 terminates this program
+  mov   X8, #93             // exit system call
   svc   #0x00		            // Call kernel to terminate the program
 
-helloworld:      .ascii  "Hello World!\n"
-
+helloworld:
+  .ascii  "Hello World!\n"
+len = . - helloworld  // Calculate length of the string
