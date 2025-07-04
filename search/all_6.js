5e173bf5e3e397621c6197f83f009d1cacdeb126
djthorpe <djthorpe@users.noreply.github.com>
2025-07-04 08:36:19 +0000

    deploy: 406c830bb6b949fcfa84eb47fc5560a52319df42

--- search/all_6.js
+++ search/all_6.js
@@ -3,6 +3,7 @@ var searchData=
   ['id',['id',['../runtime_8h.html#a0332efe4206b49a80b5028908f36de0c',1,'runtime.h']]],
   ['imp',['IMP',['../runtime_8h.html#acd98b3996b82623e9e28e3cd4507a575',1,'runtime.h']]],
   ['init',['init',['../interfaceObject.html#ad5f41da394dd6fff8a0de38dfa370420',1,'Object']]],
+  ['initialize',['initialize',['../interfaceObject.html#a0c86d32a270331881ebd5785d4c6f3f5',1,'Object']]],
   ['initwithformat_3a',['initWithFormat:',['../interfaceNXString.html#a8321b7e8defbe75e7df206483fd734a6',1,'NXString']]],
   ['initwithstring_3a',['initWithString:',['../interfaceNXString.html#aeef294233cf3d008efe5b1d286c72f8f',1,'NXString']]],
   ['isa',['isa',['../interfaceObject.html#ac697e8f00653acf23f4e018f382e3641',1,'Object']]],
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['init',['init',['../interfaceObject.html#ad5f41da394dd6fff8a0de38dfa370420',1,'Object']]],
+  ['initialize',['initialize',['../interfaceObject.html#a0c86d32a270331881ebd5785d4c6f3f5',1,'Object']]],
   ['initwithformat_3a',['initWithFormat:',['../interfaceNXString.html#a8321b7e8defbe75e7df206483fd734a6',1,'NXString']]],
   ['initwithstring_3a',['initWithString:',['../interfaceNXString.html#aeef294233cf3d008efe5b1d286c72f8f',1,'NXString']]],
   ['isequal_3a',['isEqual:',['../interfaceObject.html#af12644c1cd7498475dc61c4df7138133',1,'Object']]],
