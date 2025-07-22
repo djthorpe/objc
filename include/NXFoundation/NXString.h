a7a09c0501674d89aae4965b2e8e7de2023cd091
David Thorpe <djt@mutablelogic.com>
2025-07-22 12:08:01 +0200

    djt-0722-string

--- include/NXFoundation/NXString.h
+++ include/NXFoundation/NXString.h
@@ -5,6 +5,12 @@
  */
 #pragma once
 
+typedef enum {
+  NXComparisonAscending = -1,
+  NXComparisonSame = 0,
+  NXComparisonDescending = 1
+} NXComparisonResult;
+
 /**
  * @brief A class for managing and manipulating string objects.
  *
@@ -65,5 +71,110 @@
  * the null terminator.
  */
 - (unsigned int)length;
+
+/**
+ * @brief Appends another string to this string.
+ * @param other The string to append.
+ * @note If the string needs to be modified, it will create a mutable copy,
+ * or attempt to increase capacity if necessary. If the operation fails,
+ * it will currently panic, but should raise an exception in the future,
+ * when exception handling is implemented.
+ */
+- (void)appendString:(id)other;
+
+/**
+ * @brief Appends a string with format and arguments to this string.
+ * @param format The format string to use for appending.
+ * @param ... Variable arguments for the format string.
+ * @note If the string needs to be modified, it will create a mutable copy,
+ * or attempt to increase capacity if necessary. If the operation fails,
+ * it will currently panic, but should raise an exception in the future,
+ * when exception handling is implemented.
+ */
+- (void)appendStringWithFormat:(NXConstantString *)format, ...;
+
+/**
+ * @brief Appends a null-terminated C-string to this string.
+ * @param other The C-string to append.
+ * @note If the string needs to be modified, it will create a mutable copy,
+ * or attempt to increase capacity if necessary. If the operation fails,
+ * it will currently panic, but should raise an exception in the future,
+ * when exception handling is implemented.
+ */
+- (void)appendCString:(const char *)other;
+
+/**
+ * @brief Returns a quoted version of the string.
+ * @return A new NXString instance containing the string enclosed in
+ * double quotes.
+ *
+ * This method creates a new string that contains the original string
+ * enclosed in double quotes. Special characters within the string are escaped
+ * according to JSON string escaping rules.
+ */
+- (NXString *)quotedString;
+
+/**
+ * @brief Trims leading and trailing whitespace from the string.
+ * @return YES if the string was modified, NO if it was already trimmed.
+ */
+- (BOOL)trimSpace;
+
+/**
+ * @brief Trims leading and trailing string values from the string.
+ * @param prefix The NXConstantString or NXString instance to trim from the
+ * start, or NULL to skip.
+ * @param suffix The NXConstantString or NXString instance to trim from the
+ * end, or NULL to skip.
+ * @return YES if the string was modified, NO if it was already trimmed.
+ */
+- (BOOL)trimPrefix:(id)prefix suffix:(id)suffix;
+
+/**
+ * @brief Checks if the string starts with a given prefix.
+ * @param prefix The NXConstantString or NXString instance to check as a
+ * prefix.
+ * @return YES if the string starts with the specified prefix, NO otherwise.
+ */
+- (BOOL)hasPrefix:(id)prefix;
+
+/**
+ * @brief Checks if the string ends with a given suffix.
+ * @param suffix The NXConstantString or NXString instance to check as a
+ * suffix.
+ * @return YES if the string ends with the specified suffix, NO otherwise.
+ */
+- (BOOL)hasSuffix:(id)suffix;
+
+/**
+ * @brief Counts the number of occurrences of a character.
+ * @param ch The character to count occurrences of.
+ * @return The number of times the character appears in the string.
+ */
+- (uint32_t)countOccurrencesOfByte:(const char)ch;
+
+/**
+ * @brief Counts the number of occurrences of a string.
+ * @param other The string to count occurrences of.
+ * @return The number of times the string appears in the string.
+ */
+- (uint32_t)countOccurrencesOfString:(NXString *)other;
+
+/**
+ * @brief Checks if the string contains a given substring.
+ * @param other The NXConstantString or NXString instance to check for
+ * containment.
+ * @return YES if the string contains the specified substring, NO otherwise.
+ */
+- (BOOL)containsString:(id)other;
+
+/**
+ * @brief Compares this string with another string.
+ * @param other The string to compare against.
+ * @return NXComparisonResult indicating whether this string is earlier
+ * (Ascending), Same, or later (Descending) than the other string, according
+ * to lexicographical order.
+ */
+- (NXComparisonResult)compare:(NXString *)other;
 
 @end
