5a6917649127238e699e4f2fac0cd3cde6c52604
David Thorpe <djt@mutablelogic.com>
2025-07-23 19:48:25 +0200

    Added protocol tests - ongoing

--- src/runtime-gcc/Protocol.m
+++ src/runtime-gcc/Protocol.m
@@ -10,11 +10,32 @@
 }
 
 /**
+ * @brief Returns the description of the protocol.
+ */
+- (NXConstantString *)description {
+  return [NXConstantString stringWithCString:_name];
+}
+
+/**
  * @brief Checks if this protocol conforms to another protocol.
  */
 - (BOOL)conformsTo:(Protocol *)aProtocolObject {
   return proto_conformsTo((objc_protocol_t *)self,
                           (objc_protocol_t *)aProtocolObject);
+}
+
+/**
+ * @brief Determines if this protocol is equal to another object.
+ */
+- (BOOL)isEqual:(id)anObject {
+  if (self == anObject) {
+    return YES;
+  }
+  if (![anObject isKindOfClass:[Protocol class]]) {
+    return NO;
+  }
+  Protocol *otherProtocol = (Protocol *)anObject;
+  return strcmp(_name, otherProtocol->_name) == 0;
 }
 
 @end
