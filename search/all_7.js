1b0f0dadd5a50276c80bc9e9d0109923d2f2bd07
djthorpe <djthorpe@users.noreply.github.com>
2025-07-24 07:27:47 +0000

    deploy: d5a40819955658ae3764798781e16b6cecf58f6f

--- search/all_7.js
+++ search/all_7.js
@@ -7,6 +7,8 @@ var searchData=
   ['initwithcstring_3a',['initWithCString:',['../interfaceNXString.html#aeb374c44599158565750c46e4927f3bb',1,'NXString']]],
   ['initwithformat_3a',['initWithFormat:',['../interfaceNXString.html#a278ddef64290c1f05cb77cff96d49373',1,'NXString']]],
   ['initwithstring_3a',['initWithString:',['../interfaceNXString.html#addee4e6e812fc8f39cccc51edb204f1b',1,'NXString']]],
+  ['int16value',['int16Value',['../interfaceNXNumber.html#a45f7d544fbe3ffc47489072a90ba7621',1,'NXNumber']]],
+  ['int32value',['int32Value',['../interfaceNXNumber.html#ae1e38ca9c66330ab50a76321c4f4eb50',1,'NXNumber']]],
   ['int64value',['int64Value',['../interfaceNXNumber.html#aa4675dea0334164edab8d92740c1f4d7',1,'NXNumber']]],
   ['isa',['isa',['../interfaceObject.html#ac697e8f00653acf23f4e018f382e3641',1,'Object']]],
   ['isearlierthan_3a',['isEarlierThan:',['../interfaceNXDate.html#aa556884cab0a5016d3d1f5ccefc94512',1,'NXDate']]],
