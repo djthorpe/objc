335f433bcd598707853403149bff77aeeec9a4ee
djthorpe <djthorpe@users.noreply.github.com>
2025-07-25 09:07:58 +0000

    deploy: 078b9e95591e29318fc85edd48e01ca6eceefddd

--- search/functions_d.js
+++ search/functions_d.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['tolowercase',['toLowercase',['../interfaceNXString.html#aeeecc1e2a683aae2a7b25476deee8167',1,'NXString']]],
   ['touppercase',['toUppercase',['../interfaceNXString.html#a5837f08e476b546a89e26767571ed0f4',1,'NXString']]],
+  ['trimwhitespace',['trimWhitespace',['../interfaceNXString.html#a3eb033a2797d7c94ae51e030b94f0648',1,'NXString']]],
   ['truevalue',['trueValue',['../interfaceNXNumber.html#a09d785a02139d2620348ef839ef88dad',1,'NXNumber']]]
 ];
