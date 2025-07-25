bf6afdd2836f5f11e382570b560e9ef6afdd0fa4
djthorpe <djthorpe@users.noreply.github.com>
2025-07-25 09:54:44 +0000

    deploy: 64b42fc7609c8a9cab2eeeb0a9de953f67cb43df

--- search/all_f.js
+++ search/all_f.js
@@ -4,6 +4,7 @@ var searchData=
   ['time_2eh',['time.h',['../time_8h.html',1,'']]],
   ['tolowercase',['toLowercase',['../interfaceNXString.html#aeeecc1e2a683aae2a7b25476deee8167',1,'NXString']]],
   ['touppercase',['toUppercase',['../interfaceNXString.html#a5837f08e476b546a89e26767571ed0f4',1,'NXString']]],
+  ['trimprefix_3asuffix_3a',['trimPrefix:suffix:',['../interfaceNXString.html#a0f2fd67750213341bccd2f22a94fd324',1,'NXString']]],
   ['trimwhitespace',['trimWhitespace',['../interfaceNXString.html#a3eb033a2797d7c94ae51e030b94f0648',1,'NXString']]],
   ['truevalue',['trueValue',['../interfaceNXNumber.html#a09d785a02139d2620348ef839ef88dad',1,'NXNumber']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -10,6 +10,7 @@ var searchData=
   ['class_5frespondstoselector',['class_respondsToSelector',['../runtime_8h.html#a2fa7f9d3bdb155fb39a35714304ed104',1,'runtime.h']]],
   ['compare_3a',['compare:',['../interfaceNXDate.html#adf30381f387f21fbeb6f7fad51f077a1',1,'NXDate::compare:()'],['../interfaceNXString.html#af5014b9b35f1ce101b204e3f6af5d0f0',1,'NXString::compare:()']]],
   ['conformsto_3a',['conformsTo:',['../interfaceObject.html#a8933d0f25d908db0309a701b0f06a825',1,'Object::conformsTo:(Protocol *aProtocolObject)'],['../interfaceObject.html#a8933d0f25d908db0309a701b0f06a825',1,'Object::conformsTo:(Protocol *aProtocolObject)'],['../interfaceProtocol.html#a701d08b6e101e40950d20b53a4f7c16a',1,'Protocol::conformsTo:()']]],
+  ['containsstring_3a',['containsString:',['../interfaceNXString.html#a87abf6b4ab5c3cf25502964a6a4bf084',1,'NXString']]],
   ['countoccurrencesofbyte_3a',['countOccurrencesOfByte:',['../interfaceNXString.html#ad7de3650b5d7e7d7ff50044c3a9dc3bb',1,'NXString']]],
   ['cstr',['cStr',['../protocolNXConstantStringProtocol-p.html#a84effffa6870fb756eb604fa16b52d76',1,'NXConstantStringProtocol-p::cStr()'],['../interfaceNXConstantString.html#a0268f6065de529f52473ad4aa67cb158',1,'NXConstantString::cStr()'],['../interfaceNXString.html#ac08eac6f81ec96730cbe93a88c6e31e4',1,'NXString::cStr()']]],
   ['currentpool',['currentPool',['../interfaceNXAutoreleasePool.html#a69a65f8bbdea0ef542382fa496dbdba8',1,'NXAutoreleasePool']]]
