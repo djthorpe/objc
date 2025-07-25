25d917a52977342bd277583b11159a635ff061a0
djthorpe <djthorpe@users.noreply.github.com>
2025-07-25 08:33:12 +0000

    deploy: c45e02eef8952ebd9f6322412bbe5991f2465397

--- search/all_1.js
+++ search/all_1.js
@@ -5,6 +5,8 @@ var searchData=
   ['alloc',['alloc',['../interfaceObject.html#a57b94628f4a28ffa19da4cf50199cbaa',1,'Object']]],
   ['allocwithsize_3a',['allocWithSize:',['../interfaceNXZone.html#ad790e31d4c78534b5ceff67caee2f5f1',1,'NXZone']]],
   ['allocwithzone_3a',['allocWithZone:',['../interfaceNXObject.html#a07c1150dddd0486576a9c9af00244db5',1,'NXObject']]],
+  ['append_3a',['append:',['../interfaceNXString.html#a6173efb724785281af7e37a77026f2d8',1,'NXString']]],
+  ['appendcstring_3a',['appendCString:',['../interfaceNXString.html#a23292b71d529573fa606c2b043788e7d',1,'NXString']]],
   ['assert_2eh',['assert.h',['../assert_8h.html',1,'']]],
   ['autorelease',['autorelease',['../interfaceNXObject.html#a343832a14602dc015c936ff00922cead',1,'NXObject']]]
 ];
