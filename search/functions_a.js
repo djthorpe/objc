7be5a605ba0b43430872597eb626289668c6b265
djthorpe <djthorpe@users.noreply.github.com>
2025-07-26 18:52:34 +0000

    deploy: 9a2d577fb87939d8860dc99a6695d1d812c7b664

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['objc_5ffree',['objc_free',['../malloc_8h.html#a8f8e2ee1df5b130d4c96555fff909fdd',1,'malloc.h']]],
+  ['objc_5ffree',['objc_free',['../group__objc.html#ga8f8e2ee1df5b130d4c96555fff909fdd',1,'malloc.h']]],
   ['objc_5flookupclass',['objc_lookupClass',['../group__objc.html#gad60fc60359e0c7b9a6751f835f5537a2',1,'runtime.h']]],
-  ['objc_5fmalloc',['objc_malloc',['../malloc_8h.html#a12b32d3d80a37f14adc2bc963eb0bde7',1,'malloc.h']]],
+  ['objc_5fmalloc',['objc_malloc',['../group__objc.html#ga12b32d3d80a37f14adc2bc963eb0bde7',1,'malloc.h']]],
   ['objc_5fsync_5fenter',['objc_sync_enter',['../group__objc.html#gab912f47eca33ff595b0270360079ff38',1,'mutex.h']]],
   ['objc_5fsync_5fexit',['objc_sync_exit',['../group__objc.html#ga9acbff2a0268bf1fece869d50ae54426',1,'mutex.h']]],
   ['object_5fgetclass',['object_getClass',['../group__objc.html#ga0a52026f8341ccd035d68c2fdb091491',1,'runtime.h']]],
