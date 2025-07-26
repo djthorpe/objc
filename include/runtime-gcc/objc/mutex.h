0fbb1a41bf405310510db9e48f2aa836807f8dfa
David Thorpe <djt@mutablelogic.com>
2025-07-26 20:51:58 +0200

    Update mutex.h

--- include/runtime-gcc/objc/mutex.h
+++ include/runtime-gcc/objc/mutex.h
@@ -8,6 +8,7 @@
 
 /**
  * @brief Acquire a lock on the specified object for thread synchronization.
+ * @ingroup objc   
  * @param obj The object to lock. Must not be nil.
  * @return 0 on success, or a non-zero error code on failure.
  */
@@ -15,6 +16,7 @@ int objc_sync_enter(id obj);
 
 /**
  * @brief Release a lock on the specified object.
+ * @ingroup objc   
  * @param obj The object to unlock. Must be the same object passed to
  * objc_sync_enter().
  * @return 0 on success, or a non-zero error code on failure.
