cb4322d27fc055759bec1f4bf98498644a7adfaf
David Thorpe <djt@mutablelogic.com>
2025-07-26 20:29:21 +0200

    Update sys.h

--- include/runtime-sys/sys.h
+++ include/runtime-sys/sys.h
@@ -1,7 +1,7 @@
 /**
  * @file sys.h
  * @brief Defines system management functions.
- * @defgroup sys
+ * @defgroup System
  *
  * This file declares various system methods for managing memory, threads
  * and resources.
@@ -21,6 +21,7 @@ extern "C" {
 
 /**
  * @brief Outputs a string to the system console or standard output.
+ * @ingroup System 
  * @param str A pointer to a null-terminated string to be output. If `str` is
  * `NULL`, existing output is flushed.
  *
@@ -32,6 +33,7 @@ extern void sys_puts(const char *str);
 /**
  * @brief Outputs a character to the system console or standard output.
  * @param ch The character to be output.
+ * @ingroup System
  *
  * This function writes the specified character to the
  * system's standard output stream, but does not flush the output buffer.
@@ -40,16 +42,19 @@ extern void sys_putch(const char ch);
 
 /**
  * @brief Initializes the system on startup.
+ * @ingroup System
  */
 extern void sys_init(void);
 
 /**
  * @brief Cleans up the system on shutdown.
+ * @ingroup System
  */
 extern void sys_exit(void);
 
 /**
  * @brief Aborts the current process immediately.
+ * @ingroup System
  * @note This function does not return to the caller.
  *
  *  This function terminates the current process abnormally and
@@ -60,6 +65,7 @@ extern void sys_abort(void);
 
 /**
  * @brief Prints a formatted panic message and aborts the process.
+ * @ingroup System
  * @param fmt A printf-style format string specifying the panic message.
  * @param ... Additional arguments corresponding to format specifiers in fmt.
  * @note This function does not return to the caller.
