230b48823835646a03453bac09aed6490ac3dcc5
djthorpe <djthorpe@users.noreply.github.com>
2025-07-28 11:55:48 +0000

    deploy: 14e63d31fcebc04f82966696c9ffa15e0d9f9b29

--- search/groups_2.js
+++ search/groups_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['system_20management',['System Management',['../group__System.html',1,'']]]
+  ['system_20management',['System Management',['../group__System.html',1,'']]],
+  ['system_20events',['System Events',['../group__SystemEvents.html',1,'']]]
 ];
--- search/typedefs_6.js
+++ search/typedefs_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['sel',['SEL',['../group__objc.html#gacc50f82530a982adeb5979def1ca71fa',1,'runtime.h']]],
-  ['sys_5fevent_5ft',['sys_event_t',['../group__System.html#ga2bdd4be7e6e04b4ee95e0459ff001702',1,'event.h']]],
+  ['sys_5fevent_5ft',['sys_event_t',['../group__SystemEvents.html#ga2bdd4be7e6e04b4ee95e0459ff001702',1,'event.h']]],
   ['sys_5fthread_5ffunc_5ft',['sys_thread_func_t',['../group__System.html#ga44d92420e9c1148c746ce7e89a81075d',1,'thread.h']]],
   ['sys_5ftime_5ft',['sys_time_t',['../group__System.html#ga45596b2dc3b59917dfbbf36d5b3d4816',1,'time.h']]],
   ['sys_5ftimer_5ft',['sys_timer_t',['../group__System.html#ga3533996feca2a9c8233da4b172f4c007',1,'timer.h']]]
