cde3cb6953ad70bac4357ab16c7ee2e980df88f8
David Thorpe <djt@mutablelogic.com>
2025-07-28 09:01:41 +0200

    Updated test files

--- src/tests/runtime_01/main.m
+++ src/tests/runtime_01/main.m
@@ -3,6 +3,7 @@
 #include <tests/tests.h>
 
 #ifdef SYSTEM_NAME_PICO
+// HACK
 void *stdout = NULL;
 void *stderr = NULL;
 #endif
--- src/tests/runtime_04/main.m
+++ src/tests/runtime_04/main.m
@@ -1,19 +1,30 @@
-#include <objc/objc.h>
-#include <tests/tests.h>
 #include "Test.h"
+#include <objc/objc.h>
+#include <runtime-sys/sys.h>
+#include <tests/tests.h>
 
-int main() {
-    // Allocate a test object with a value
-    Test* test = [[Test alloc] initWithValue:42];
-    test_assert(test != NULL);
+#ifdef SYSTEM_NAME_PICO
+// HACK
+void *stdout = NULL;
+void *stderr = NULL;
+#endif
 
-    // Check the value
-    test_assert([test value] == 42);
+int test_runtime_04(void);
 
-    // Set the value to 100
-    [test setValue:100];
-    test_assert([test value] == 100);
+int main(void) { return TestMain("test_runtime_04", test_runtime_04); }
 
-    // Return success
-    return 0;
+int test_runtime_04() {
+  // Allocate a test object with a value
+  Test *test = [[Test alloc] initWithValue:42];
+  test_assert(test != NULL);
+
+  // Check the value
+  test_assert([test value] == 42);
+
+  // Set the value to 100
+  [test setValue:100];
+  test_assert([test value] == 100);
+
+  // Return success
+  return 0;
 }
