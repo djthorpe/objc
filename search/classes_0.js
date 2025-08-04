a192aaa3c0418d92c6dabf8cf1a6312f0eba3fcd
djthorpe <djthorpe@users.noreply.github.com>
2025-08-04 12:03:04 +0000

    deploy: 7b633af2f96b85f1bb35df4b1fd5acc8a92c72ed

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['application',['Application',['../interfaceApplication.html',1,'']]],
-  ['applicationdelegate_20_2dp',['ApplicationDelegate -p',['../protocolApplicationDelegate_01-p.html',1,'']]],
   ['applicationdelegate_2dp',['ApplicationDelegate-p',['../protocolApplicationDelegate-p.html',1,'']]]
 ];
