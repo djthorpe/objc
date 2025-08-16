a20147494e708fd8a49135e98997b9d62a78a70f
djthorpe <djthorpe@users.noreply.github.com>
2025-08-16 17:06:51 +0000

    deploy: 1827874a868844d6ab2c15f1eeb3c8e658052109

--- search/groups_a.js
+++ search/groups_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['network_20interfaces',['Network Interfaces',['../group__Network.html',1,'']]],
-  ['network_20time_20protocol',['Network Time Protocol',['../group__NTP.html',1,'']]]
+  ['network_20framework',['Network Framework',['../group__Network.html',1,'']]],
+  ['network_20interfaces',['Network Interfaces',['../group__RuntimeNet.html',1,'']]],
+  ['network_20time_20protocol',['Network Time Protocol',['../group__RuntimeNTP.html',1,'']]]
 ];
