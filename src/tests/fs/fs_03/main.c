5857c003d3da53bd4087d822088cb98bbb3cb9be
David Thorpe <djt@mutablelogic.com>
2025-09-06 10:36:28 +0200

    Updated

--- src/tests/fs/fs_03/main.c
+++ src/tests/fs/fs_03/main.c
@@ -10,7 +10,7 @@
 // TEST
 
 int test_fs_03(void) {
-  size_t size = 1024 * 64;
+  size_t size = 1024 * 10;
   const char *path = "/tmp/test_fs_03_volume.img";
 
   // Create a file-backed file system volume
@@ -33,6 +33,12 @@ int test_fs_03(void) {
     sys_printf("name='%s' type=%-4s size=%zu path='%s'\n", file.name,
                file.dir ? "DIR" : "FILE", file.size, file.path);
   }
+
+  // Print total size and used size
+  size_t free_bytes = 0;
+  size_t total_bytes = fs_vol_size(volume, &free_bytes);
+  sys_printf("Volume size: total=%zu bytes, free=%zu bytes, used=%zu bytes\n",
+             total_bytes, free_bytes, total_bytes - free_bytes);
 
   // Finalize the volume
   fs_vol_finalize(volume);
