5172bcfdc2fce66065093fbe8dfe3a6a4e086c2c
David Thorpe <djt@mutablelogic.com>
2025-09-09 15:53:30 +0200

    Updated function signatures

--- src/runtime-gcc/dtable.h
+++ src/runtime-gcc/dtable.h
@@ -12,9 +12,9 @@ bool _objc_dtable_init(Class cls);
 /*
  * Check whether an IMP exists in the dispatch table
  */
-bool _objc_dtable_exists(Class cls, IMP imp);
+bool _objc_dtable_exists(Class cls, void *sel_id);
 
 /*
  * Add an IMP to the dispatch table (linear probing). No deletes supported.
  */
-bool _objc_dtable_add(Class cls, IMP imp);
+bool _objc_dtable_add(Class cls, void *sel_id);
